036a373f58f32ea33e531cc7a1174f408ab8b9b9
Learning-howto-Code <jaketorohopkins@icloud.com>
2025-08-06 11:26:09 -0700

    started laying out the buttons

--- main.py
+++ main.py
@@ -1,18 +1,47 @@
-#Import the required Libraries
-from tkinter import *
-from tkinter import ttk
-#Create an instance of Tkinter frame
-win = Tk()
-#Set the geometry of Tkinter frame
-win.geometry("750x270")
+import tkinter as tk
+import tkinter.ttk as ttk
 
-def open_popup():
-   top= Toplevel(win)
-   top.geometry("750x250")
-   top.title("Child Window")
-   Label(top, text= "Hello World!", font=('Mistral 18 bold')).place(x=150,y=80)
+window = tk.Tk()
+idea= "get a glass of water"
 
-Label(win, text=" Click the Below Button to Open the Popup Window", font=('Helvetica 14 bold')).pack(pady=20)
-#Create a button in the main Window to open the popup
-ttk.Button(win, text= "Open", command= open_popup).pack()
-win.mainloop()
+#calls frames(like a grid)
+top_frame = tk.Frame(master=window, width=500, height=200, )
+button_frame = tk.Frame(master=window, width=500, height=200)
+
+Title = tk.Label(top_frame,text="Get back to Realtiy!",font=("Arial", 34))
+break_message = tk.Label(top_frame,text="Take a quick 5 minute break",font=("Arial", 22))
+break_idea=tk.Label(top_frame,text=idea,font=("Arial", 22))
+
+#updates #completed counter
+def read_counter(): # gets number fromt txt file as var
+    with open("counter.txt") as f: #reads counter.txt
+        counter = int(f.read()) #converts to int
+        print(counter)
+        return counter
+def update_counter(): #adds onto var
+    counter = read_counter()
+    with open("counter.txt", "w") as f:
+        counter+= 1
+        f.write(str(counter)) # adds one to counter, then converts to str
+
+#buttons
+completed = tk.Button(button_frame, text="Did it!", command=update_counter, width=8)
+skipped= tk.Button(button_frame, text="Skipped it", width=8)
+
+#places items in frame
+top_frame.grid(row=0, column=1)
+button_frame.grid(row=4, column=1)
+button_frame.grid_propagate(False)
+button_frame.grid_columnconfigure(1, weight=2)
+
+#items in top frame
+Title.grid(row=0, column=0)
+break_message.grid(row=1, column=0)
+break_idea.grid(row=2, column=0)
+
+# items in button frame
+completed.grid(row=3, column=0, padx=50)
+skipped.grid(row=3, column= 2, padx=50)
+
+window.mainloop()
+
