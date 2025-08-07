54effb8bf1ae74925742cf2f8384c2c462142de7
Learning-howto-Code <jaketorohopkins@icloud.com>
2025-08-07 14:20:12 -0700

    added link to repo

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # screen-break
-A firendly little app that reminds you to take a break every hour
+A friednly little app that reminds you to take a break every hour.
+
 built with TKinter and python Screen Break opens a pop up window every hour with a firendly remidner to take a break, a recomendation for what to do and a streak counter.
 Works on mac, windows and linux, but only tested on mac
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+url="https://github.com/Learning-howto-Code/screen-break/tree/main"
+
 import tkinter as tk
 import time
-
+import webbrowser
 
 window = tk.Tk()
-
 
 #updates #completed counter
 def read_counter(): # gets number fromt txt file as var
@@ -26,31 +27,36 @@ def delete_window():# becuase buttons can only call funtions
     time.sleep(.3)
     window.destroy()
 def ideas(): #returns the idea nuber in the list that corsponds to the number in ideas number.txt
+   
    with open("ideas.txt") as f: #reads ideas.txt 
     ideas = f.readlines()
-    print(ideas)
+    num_lines=len(ideas)
     with open("idea#.txt") as f: # reads ideas to figure out where we left off
      number = int(f.read().strip())
     with open("idea#.txt", "w") as f:
         number+= 1
-        if number>23:
+        if number>=num_lines:
            number=0
         f.write(str(number))
     final_idea = ideas[number] # prints the idea in the list that the var number is at
     print(number)
     return final_idea
+def openlink(event=None):
+   webbrowser.open_new(url)
 final_idea = ideas()
 
 #calls frames(like a grid)
 top_frame = tk.Frame(master=window, width=500, height=200, bg="#2E3440" )
 button_frame = tk.Frame(master=window, width=500, height=200, bg="#2E3440")
 amount_completed=tk.Frame(master=window, width=500, height=200, bg="#2E3440")
+link_text=tk.Frame(master=window, width=500, height=200, bg="#2E3440")
 
+#labels
 Title = tk.Label(top_frame,text="Get back to Reality!",font=("Arial Rounded MT Bold", 34), bg="#2E3440",fg="#c1d3fe" )
 break_message = tk.Label(top_frame,text="Take a quick break",font=("Comic Sans MS", 22), bg="#2E3440",fg="#c1d3fe")
 break_idea=tk.Label(top_frame,text=final_idea,font=("Comic Sans MS", 22), bg="#2E3440",fg="#c1d3fe")
 completed_text=tk.Label(amount_completed, text=f"You've completed {counter} side quests!", font=("Comic Sans MS", 22), bg="#2E3440",fg="#c1d3fe")
-
+repo_link=tk.Label(amount_completed, text="made with :3 by Jake",font=("Comic Sans MS", 12), bg="#2E3440",fg="white", cursor="hand2")
 
 #buttons
 completed = tk.Button(button_frame, text="Did it!", command=update_counter, width=8,height=1,fg="#434957",font=("Comic Sans MS", 12))
@@ -62,6 +68,11 @@ button_frame.grid(row=1, column=1)
 button_frame.grid_propagate(False)
 button_frame.grid_columnconfigure(1, weight=2)
 amount_completed.grid(row=1,column=1)
+amount_completed.grid_rowconfigure(3,weight=1)
+link_text.grid(row=5, column=1)
+repo_link.grid(row=4,column=1)
+
+repo_link.bind("<Button-1>", lambda e: openlink(url))
 
 #items in top frame
 Title.grid(row=0, column=0, padx=10, pady=20)
@@ -74,23 +85,20 @@ skipped.grid(row=2, column= 2, padx=50)
 
 #items in amount completed frame
 completed_text.grid(row=1, column=1, padx=10)
-
 #calls size of pop up window
 window_width=500
 window_height=300
-
 #gets size of screen
 screen_width=window.winfo_screenwidth()
 screen_height=window.winfo_screenheight()
-
 #finds middle of window
 x = int((screen_width - window_width) / 2)
 y = int((screen_height - window_height) / 2)
-
 window.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
 window.resizable(False, False)
 window.configure(bg="#2E3440")
+window.title("Screen Break")
 window.mainloop()
 
 
